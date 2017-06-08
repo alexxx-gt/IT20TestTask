cf4123bc81bd4ca4a912a87775e5cdf1d856a3c9
Александр <alexxx-gt@yandex.ru>
2017-06-08 07:45:01 +0300

    Added project with Auth for migration to it

---
+++ IT20TestTaskAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IT20TestTaskAuth.Startup))]
+namespace IT20TestTaskAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
